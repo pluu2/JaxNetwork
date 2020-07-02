22321dda847983fb6f3efc83c8f2ffddcc035d68
pluu <57818677+pluu2@users.noreply.github.com>
2020-07-02 16:03:40 -0400

    Create jax_base_model.py

---
+++ jax_base_model.py
@@ -0,0 +1,31 @@
+class jax_base_model(): 
+  def __init__(self): 
+    self.layer=[]
+    
+
+  def relu (self,x,axis=0): 
+    return np.max([np.zeros((np.size(x,axis=0),np.size(x,axis=1))) ,x],axis=0)
+
+  def sigmoid_act(self,x): 
+    return (np.exp(x)/(np.exp(x)+1))
+
+  def tanh_act(self,x): 
+    return np.tanh(x)
+
+  def trainable_variables(self):  
+    trainable_v={}
+    for i in range(self.num_layers): 
+      trainable_v[str(i)+' weights'] = self.layer[i].weights
+      trainable_v[str(i)+' bias'] = self.layer[i].bias
+    return trainable_v
+
+  def binary_crossentropy(self,x,y): #x=input, y= target
+    return -y*np.log(x)-(1-y)*np.log(1-x)
+
+  def update_weights (self,parameters,lr=1.0):
+    for i in range(self.num_layers):
+      self.layer[i].weights=self.layer[i].weights - lr*parameters[str(i) + ' weights'] 
+      self.layer[i].bias=self.layer[i].bias - lr*parameters[str(i) + ' bias']
+  
+  def gradient(self,parameters): 
+    return grad(self.backwards)(parameters)
